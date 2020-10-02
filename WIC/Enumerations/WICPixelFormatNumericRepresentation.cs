3a25bff4abcc9d1b0cbed89af07b65c7f1d751e2
Jimbobicus <jdonald@muellerwp.com>
2020-10-02 12:11:42 -0700

    add pf info2

---
+++ WIC/Enumerations/WICPixelFormatNumericRepresentation.cs
@@ -0,0 +1,12 @@
+namespace WIC
+{
+    public enum WICPixelFormatNumericRepresentation : int
+    {
+        Unspecified = 0x0,
+        Indexed = 0x1,
+        UnsignedInteger = 0x2,
+        SignedInteger = 0x3,
+        Fixed = 0x4,
+        Float = 0x5,
+    }
+}
---
+++ WIC/Interfaces/IWICPixelFormatInfo2.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace WIC
+{
+    [ComImport]
+    [Guid(IID.IWICPixelFormatInfo2)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IWICPixelFormatInfo2 : IWICPixelFormatInfo
+    {
+        #region Members inherited from `IWICComponentInfo`
+
+        new WICComponentType GetComponentType();
+
+        new Guid GetCLSID();
+
+        new WICComponentSigning GetSigningStatus();
+
+        new void GetAuthor(
+            [In] int cchAuthor,
+            [In, Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2, SizeParamIndex = 0)] char[] wzAuthor,
+            [Out] out int pcchActual);
+
+        new Guid GetVendorGUID();
+
+        new void GetVersion(
+            [In] int cchVersion,
+            [In, Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2, SizeParamIndex = 0)] char[] wzVersion,
+            [Out] out int pcchActual);
+
+        new void GetSpecVersion(
+            [In] int cchSpecVersion,
+            [In, Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2, SizeParamIndex = 0)] char[] wzSpecVersion,
+            [Out] out int pcchActual);
+
+        new void GetFriendlyName(
+            [In] int cchFriendlyName,
+            [In, Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2, SizeParamIndex = 0)] char[] wzFriendlyName,
+            [Out] out int pcchActual);
+
+        #endregion
+
+        #region Members inherited from `IWICPixelFormatInfo`
+        
+        new Guid GetFormatGUID();
+
+        new IWICColorContext GetColorContext();
+
+        new int GetBitsPerPixel();
+
+        new int GetChannelCount();
+
+        new void GetChannelMask(
+            [In] int uiChannelIndex,
+            [In] int cbMaskBuffer,
+            [In, Out, MarshalAs(UnmanagedType.LPArray, ArraySubType=UnmanagedType.U1, SizeParamIndex=1)] byte[] pbMaskBuffer,
+            [Out] out int pcbActual);
+        
+        #endregion
+
+        bool SupportsTransparency();
+
+        WICPixelFormatNumericRepresentation GetNumericRepresentation();
+    }
+}
